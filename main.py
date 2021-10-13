0bfaf99bec5fdb798daf1df08821a9fd5a246b0c
Aman Singh <68653906+Gagarod@users.noreply.github.com>
2021-10-14 03:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 global board
-board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-player = "X"
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+player = 'X'
 
 
 def printBoard(board):
     for line in board:
-        print(line)
+        print line
 
 
 def makeMove():
     global player
-    x = int(input("Player " + player + ", What is the X coordinate? "))
-    y = int(input("Player " + player + ", What is the Y coordinate? "))
-    while board[y][x] != " ":
-        print("You must choosse an empty spot!")
-        x = int(input("Player " + player + ", What is the X coordinate? "))
-        y = int(input("Player " + player + ", What is the Y coordinate? "))
+    x = int(input('Player ' + player + ', What is the X coordinate? '))
+    y = int(input('Player ' + player + ', What is the Y coordinate? '))
+    while board[y][x] != ' ':
+        print 'You must choosse an empty spot!'
+        x = int(input('Player ' + player
+                + ', What is the X coordinate? '))
+        y = int(input('Player ' + player
+                + ', What is the Y coordinate? '))
 
-    if player == "X":
-        board[y][x] = "X"
-        player = "O"
+    if player == 'X':
+        board[y][x] = 'X'
+        player = 'O'
     else:
-        board[y][x] = "O"
-        player = "X"
+        board[y][x] = 'O'
+        player = 'X'
 
 
 def isWin():
     global player
-    if player == "X":
-        p = "O"
+    if player == 'X':
+        p = 'O'
     else:
-        p = "X"
+        p = 'X'
     for c in range(len(board)):
         win = True
         for r in range(len(board)):
@@ -46,24 +48,25 @@ def isWin():
                 win = False
                 break
         if win:
-            return True        
-    win=True
+            return True
+    win = True
     for c in range(len(board)):
-      if board[c][c]!=p:
-        win=False
-        break
+        if board[c][c] != p:
+            win = False
+            break
     if win:
-      return True
-    win=True
+        return True
+    win = True
 
     for r in range(len(board)):
-      if board[r][len(board)-1-r]!=p:
-        win=False
-        break
+        if board[r][len(board) - 1 - r] != p:
+            win = False
+            break
     if win:
-      return True      
-    
+        return True
+
     return False
+
 
 def main():
     gamewon = False
@@ -71,7 +74,8 @@ def main():
         printBoard(board)
         makeMove()
         gamewon = isWin()
-    print("GAMEOVER")
-    printBoard(board)    
+    print 'GAMEOVER'
+    printBoard(board)
+
 
 main()
